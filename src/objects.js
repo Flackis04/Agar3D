953b49cfba0b4caef12608c20f5d1bb4cd404685
Flackis04 <superblixt@gmail.com>
2026-06-05 23:25:40 +0200

    Revert "Set arena size and pellet count"
    This reverts commit 0dc3e77881d99841b405edf5ad0eca2cc09d416f.

--- src/objects.js
+++ src/objects.js
@@ -3,8 +3,8 @@ import * as THREE from "three";
 import * as BufferGeometryUtils from "three/addons/utils/BufferGeometryUtils.js";
 import { SpatialGrid } from "./utils/spatialGrid.js";
 
-export const mapSize = 125;
-export const pelletCount = 125000;
+export const mapSize = 250;
+export const pelletCount = 100000;
 export const pelletMinSize = 0.03;
 export const pelletMaxSize = 0.04;
 export const minBetUsd = 5;
